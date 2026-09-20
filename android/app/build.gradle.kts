e1c5337c2314d00d45484996e45becb61494c442
cypherdream <brianlexyo547@gmail.com>
2026-09-20 07:15:44 +0300

    Migrate Kotlin compiler options DSL

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = "17"
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
